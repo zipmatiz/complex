8e214d43e41aa4d7e836ba5279a39b1beb5f77f9
dadmin <dadmin@corp.local>
2019-09-22 20:32:23 +0100

    typo body-parser server/index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const keys = require('./keys');
 
 // Express App Setup
 const express = require('express');
-const bodyParser = require('bodyParser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
