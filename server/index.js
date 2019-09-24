3f096d3d8cc080e9309e68aea4d413933dc6209d
dadmin <dadmin@corp.local>
2019-09-24 22:32:53 +0100

    added handler for index = 0 in server/index.js file

--- server/index.js
+++ server/index.js
@@ -54,6 +54,10 @@ app.get('/values/current', async (req, res) => {
 app.post('/values', async (req, res) => {
   const index = req.body.index;
 
+  if (parseInt(index) = 0) {
+    return res.status(422).send('Index not valid');
+  }
+
   if (parseInt(index) > 40) {
     return res.status(422).send('Index too high');
   }
